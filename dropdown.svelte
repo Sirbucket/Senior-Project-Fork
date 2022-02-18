09b32f7dc0fdb6e230e35ee6457fe1a924b255be
Ian <ianblanchet01@gmail.com>
2022-02-18 08:54:43 -0500

    commit

--- dropdown.svelte
+++ dropdown.svelte
@@ -1,11 +1,49 @@
 <script>
-
+    function myFunction(x) {
+        x.classList.toggle('change')
+    }
 </script>
 
 <html lang=en>
-    
+    <div class="container" onclick="myFunction(this)">
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+    </div>
 </html>
 
 <style>
+html {
+    width: 40px;
+    height: 40px;
+}
+.container {
+  display: inline-block;
+  cursor: pointer;
+}
 
+.bar1, .bar2, .bar3 {
+  width: 35px;
+  height: 5px;
+  background-color: #333;
+  margin: 6px 0;
+  transition: 0.4s;
+}
+
+/* Rotate first bar */
+.change .bar1 {
+  -webkit-transform: rotate(-45deg) translate(-9px, 6px) ;
+  transform: rotate(-45deg) translate(-9px, 6px) ;
+}
+
+/* Fade out the second bar */
+.change .bar2 {
+  opacity: 0;
+}
+
+/* Rotate last bar */
+.change .bar3 {
+  -webkit-transform: rotate(45deg) translate(-8px, -8px) ;
+  transform: rotate(45deg) translate(-8px, -8px) ;
+}
 </style>
