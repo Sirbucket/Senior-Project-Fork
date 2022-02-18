0936762f743b2af26b600385b6aac65cec7c3e03
Ian <ianblanchet01@gmail.com>
2022-02-18 08:54:38 -0500

    commit

--- colors.svelte
+++ colors.svelte
@@ -1,11 +1,41 @@
 <script>
+    function styleChange() {
 
+    }
 </script>
 
 <html lang=en>
-    
+    <!--
+    <div class="title">
+      <p>Website Appearance</p>
+    </div>
+    <div id="classy" class="flexy">
+      <button class="classyB" on:click={styleChange}></button>
+      <p>Classy</p>
+    </div>
+    <div id="cozy" class="flexy">
+      <button class="cozyB" on:click={styleChange}></button>
+      <p>Cozy</p>
+    </div>
+    <div id="cool" class="flexy">
+      <button class="coolB" on:click={styleChange}></button>
+      <p>Cool</p>
+    </div>
+    <div id="carefree" class="flexy">
+      <button class="carefreeB" on:click={styleChange}></button>
+      <p>Carefree</p>
+    </div>
+    <div id="celestial" class="flexy">
+      <button class="celestialB" on:click={styleChange}></button>
+      <p>Celestial</p>
+    </div>
+-->
 </html>
 
 <style>
-
+html {
+    width: 120px;
+    height: 230px;
+    background-color: red;
+}
 </style>
--- header.svelte
+++ header.svelte
@@ -1,9 +1,17 @@
 <script>
-
+    import Colors from './colors.svelte'
+    import Dropdown from './dropdown.svelte'
 </script>
 
 <html lang=en>
-    
+    <main>
+        <Dropdown/>
+        <div class="text">
+            <h1>Chancellor's Counselling Cafe</h1>
+            <h3>"Come on in! Take a chance." - Katherine Stout</h3>
+        </div>
+        <Colors/>
+    </main>
 </html>
 
 <style>
